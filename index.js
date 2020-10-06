7457cb8631678f040a680477fe5fc4eb0092a265
Irene <irene@buddybid.com>
2020-10-06 16:33:28 +1300

    Changed if state

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const validateTag = (markup) => {
 	const stack = [];
 	let htmlText = markup.replace(/<[^>]*\/\s?>/g, '');
 	htmlText = htmlText.replace(/<[^A-Z\/][a-z\D\d.\s]+?>/g, '');
-	
+
 	const allTags = htmlText.match(/<.*?>/g);
-	
+
 	let result = null;
-	
+
 	for (let i = 0; i < allTags.length; i++) {
 		let isValid = isValidTag(allTags[i]);
 		/* can be improve */
@@ -23,28 +23,36 @@ const validateTag = (markup) => {
 				// Add all open tag to stack
 				stack.push(tagKey);
 			} else {
-				if (stack.length > 0 && stack[stack.length - 1] !== tagKey) {
-					// wrong nesting case
-					result = {
-						success: false,
-						expected: `</${stack[stack.length - 1]}>`,
-						unexpected: allTags[i],
-						message: `Expected </${stack[stack.length - 1]}> found ${allTags[i]}`
-					};
-					break;
-				} else if (!_.includes(stack, tagKey)) {
-					// Missing open tag
-					result = {
-						success: false,
-						expected: '#',
-						unexpected: allTags[i],
-						message: `Expected # found ${allTags[i]}`
-					};
-					break;
+				let stackLength = stack.length > 0 ? stack.length-1 : 0;
+
+				if (stack[stackLength] !== tagKey) {
+					if (stack[stackLength]){
+						// if last open tag is not equal to close tag
+						// wrong nesting case
+						result = {
+							success: false,
+							expected: `</${stack[stackLength]}>`,
+							unexpected: allTags[i],
+							message: `Expected </${stack[stack.length - 1]}> found ${allTags[i]}`
+						};
+						break;
+					} else {
+						// Missing open tag
+						// if close tag match
+						result = {
+							success: false,
+							expected: '#',
+							unexpected: allTags[i],
+							message: `Expected # found ${allTags[i]}`
+						};
+						break;
+					}
+
 				} else {
 					// Remove tag from stack if closing tag is found
 					_.pullAt(stack, stack.length - 1);
 				}
+
 			}
 		} else {
 			// Tag is invalid system don't know whether it's an open or a close tag
@@ -52,7 +60,7 @@ const validateTag = (markup) => {
 			break;
 		}
 	}
-	
+
 	if (_.isEmpty(stack) && result == null) {
 		result = {success: true, message: 'Correctly tagged paragraph'};
 	} else if (stack.length > 0 && result == null) {
@@ -64,14 +72,14 @@ const validateTag = (markup) => {
 			message: `Expected </${stack[0]}> found #`
 		};
 	}
-	
+
 	return result;
 };
 
 exports.handler = async (event, context, callback) => {
-	
+
 	if (!event || (event && !event.body)) return callback({message: 'Markup is required'});
-	
+
 	let validate = validateTag(event.body);
 	if (!validate.success) {
 		// return error response
@@ -89,5 +97,5 @@ exports.handler = async (event, context, callback) => {
 		};
 		return callback(null, response);
 	}
-	
+
 };
--- index.test.js
+++ index.test.js
@@ -29,7 +29,7 @@ test('Expect this test case to pass', async (done) => {
         if (err) {
             done(err);
         }
-        
+
         expect(response.statusCode).toEqual(200);
         expect(typeof response.body).toBe("string");
         let body = JSON.parse(response.body);
@@ -111,15 +111,31 @@ test('Catch error missing close tag', async (done) => {
     return await tagChecker.handler(event, context, callback);
 });
 
-test('Catch error missing invalid tag', async (done) => {
+test('Catch error invalid tag', async (done) => {
     let event = {
         body: '<B><C>This should be centred and in boldface, <B\/B>but there is a missing closing tag</C></B>'
     };
-    
+
     const callback = (err, response) => {
         if (err) {
             expect(err).toEqual({
                 message: 'Found invalid tag <B\/B>'
+            });
+            done();
+        }
+    };
+    return await tagChecker.handler(event, context, callback);
+});
+
+test('Catch error missing open tag', async (done) => {
+    let event = {
+        body: 'This should be centred and in boldface, but there is a missing closing tag</C></B>'
+    };
+
+    const callback = (err, response) => {
+        if (err) {
+            expect(err).toEqual({
+                message: 'Expected # found <\/C>'
             });
             done();
         }
